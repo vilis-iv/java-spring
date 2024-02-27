496c026017fe475573f36b940d859dc2f13148c1
vilis-iv <sarcastrope@gmail.com>
2024-02-27 15:32:40 +0700

    init padila restapi

---
+++ pagila-restapi/src/main/java/com/personal/pagilarestapi/PagilaRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pagilarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagilaRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagilaRestapiApplication.class, args);
+	}
+
+}
